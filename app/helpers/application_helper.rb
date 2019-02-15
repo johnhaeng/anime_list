8440732e1825d07119460a1907c937f803cbf1e5
Jesus <jesus.jg.medina@gmail.com>
2019-02-15 10:04:13 -0500

    soos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,7 @@
 module ApplicationHelper
-  module ApplicationHelper
-  def get_random_image
-    image_path_prefix = "app/assets/images/"
-    image_files = Dir.glob("#{image_path_prefix}project_wall/*")
-    image_files.sample.split(image_path_prefix)[1]
+    def get_random_image
+      image_path_prefix = "app/assets/images/"
+      image_files = Dir.glob("#{image_path_prefix}project_wall/*")
+      image_files.sample.split(image_path_prefix)[1]
   end
-end
 end
